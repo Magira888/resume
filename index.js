968ed020cf53a3e215ab5539d8d69ce168bd5561
Magira888 <magiraaskarbekova@gmail.com>
2023-03-24 01:54:56 +0600

    OThis reverts commit 5ef92c4ffb45c0e0047823b5791dca62110a5e52.

--- index.js
+++
@@ -1,8 +0,0 @@
-AOS.init();
-
-const burgerIcon = document.querySelector(".burger__icon");
-const mobileMenu = document.querySelector(".mobile-menu__item");
-
-burgerIcon.addEventListener("click", () => {
-  mobileMenu.classList.toggle("show-menu");
-});
